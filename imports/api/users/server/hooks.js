24aca93093ff9ac8d9f4705dbfb8c0865372f27e
devagrawal09 <devagrawal09@gmail.com>
2018-07-10 04:41:45 +0530

    add email to database from oauth

--- imports/api/users/server/hooks.js
+++ imports/api/users/server/hooks.js
@@ -5,15 +5,17 @@ import { Caregivers } from '../../caregivers';
 
 const welcomeEmailHtml = Assets.getText('welcome-email.html');
 
-Accounts.onCreateUser(function( options, user ){          //create new caregivers
+Accounts.onCreateUser(function( options, user ) {       //create new caregivers
 
-    let first = last = '';
+    let first = last = email = '';
     if( user.services.facebook ) {
         first = user.services.facebook.first_name;
-        last = user.services.facebook.last_name;        
+        last = user.services.facebook.last_name;
+        email = user.services.facebook.email;
     } else {
         first = options.profile.firstName;
         last = options.profile.lastName;
+        email = options.profile.emailAddress;
     }
 
     if( options.profile.type === 'caregiver') {
@@ -33,15 +35,18 @@ Accounts.onCreateUser(function( options, user ){          //create new caregiver
     user.profile = options.profile;
     user.numbers = [];
 
-    if( user.emails ) {
-        Email.send({ 
-            from: 'info@healthylovedones.com', 
-            to: user.emails[0].address,
-            replyTo: 'info@healthylovedones.com',
-            subject: 'Welcome to HealthyLovedOnes',
-            html: welcomeEmailHtml
-        });
+    if( !user.emails ) {
+        if( email ) user.emails = [{ address: email, verified: true }];
+        else return user;
     }
+
+    Email.send({ 
+        from: 'info@healthylovedones.com', 
+        to: user.emails[0].address,
+        replyTo: 'info@healthylovedones.com',
+        subject: 'Welcome to HealthyLovedOnes',
+        html: welcomeEmailHtml
+    });
 
     return user;
 });
