3195bdb37dc75b4a173e82fd3ca07160a796be18
devagrawal09 <devagrawal09@gmail.com>
2018-08-01 14:10:14 +0530

    video and new zendesk

--- imports/config/client/index.js
+++ imports/config/client/index.js
@@ -3,5 +3,5 @@ import './radio-inline-template.html';
 Template.atRadioInputInline.replaces('atRadioInput');
 
 Template.body.onRendered(function() {
-    $('body').append(`<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c8ac5d56-de44-41fb-8f7d-9bc29c7ceb13"></script>`);
+    $('body').append(`<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=7d82c0a9-aa1f-41cf-9544-cc628fbd2ac4"></script>`);
 });
