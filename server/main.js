76fabf3eb1803725133f7d2039f52d7519671cbd
devagrawal09 <devagrawal09@gmail.com>
2018-09-29 05:17:23 -0400

    bugfix

--- server/main.js
+++ server/main.js
@@ -4,12 +4,12 @@ import { Caregivers } from '../imports/api/caregivers';
 const updateBgAndEdu = ()=> {
     console.log('Starting background and education data update');
     Caregivers.find({}).fetch().forEach( caregiver=> {
-        if( !caregiver.background[0] ) {
+        if( caregiver.background && !caregiver.background[0] ) {
             Caregivers.update( caregiver._id, {
                 $set: { background: [] }
             });
         }
-        if( !caregiver.education[0] ) {
+        if( caregiver.education && !caregiver.education[0] ) {
             Caregivers.update( caregiver._id, {
                 $set: { education: [] }
             });
